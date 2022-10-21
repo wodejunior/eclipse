958338085ae15415e1b5d39a6c3bb16fad2a3d80
Wodeglei Júnior Cândido da Silva <wodejunior@gmail.com>
2022-10-21 18:59:21 -0300

    Adicionei arquivo via upload

---
+++ ProjetoHelloGitHub/src/br/com/wodejunior/ProjetoGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.wodejunior;
+
+/**
+ * @author wodej
+ *
+ */
+public class ProjetoGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Integrar o projeto no GitHub.");
+		System.out.println("Hello Git");
+	}
+
+}
